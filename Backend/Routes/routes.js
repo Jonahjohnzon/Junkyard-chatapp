d825a5132f4a83bfd22365b2ecd39ec97c456602
Emedo586 <emedopeace99@gmail.com>
2024-06-28 04:12:31 -0700

    Chat app

--- Backend/Routes/routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const {getRoom, findRoom} = require('./controller')
-
-router.get("/getRoom/:id", getRoom)
-router.get("/totRoom", findRoom)
-
-module.exports = router
