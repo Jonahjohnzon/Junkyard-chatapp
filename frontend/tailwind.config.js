e8edc7274b7ead3e5e4f633f29f2be9ae3c81157
mid night <mid9it1@gmail.com>
2024-06-29 12:56:08 +0100

    test push

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,13 +6,45 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'black':"#000000",
+      'white': '#ffffff',
+      'purple': '#BF00FF',
+      'midnight': '#18171B',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'body':"#0E0E10",
+      'gray':"#AEAEAF"
+    },
+    screens: {
+      'xs': '340px',
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+
+      '3xl': '1700px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar'),
+  ],
 };
