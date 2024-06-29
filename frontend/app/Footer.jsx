1f04a4559b74413cbaa59ad97a87b8cf71844a94
mid night <mid9it1@gmail.com>
2024-06-29 12:47:19 +0100

    Footer push

---
+++ frontend/app/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className=" bg-body text-white text-sm pt-10 w-full flex justify-center items-center">
+        <p>&copy; copyright junkyard.com.ng - All rights reserved </p>
+    </div>
+  )
+}
+
+export default Footer
